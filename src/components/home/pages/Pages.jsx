23c386992f2a18cbfdd04b9c09644106b9c9e0cb
timfung <thinhpt.sp@gmail.com>
2022-10-30 14:15:57 +0700

    update

--- src/components/home/pages/Pages.jsx
+++ src/components/home/pages/Pages.jsx
@@ -2,25 +2,13 @@ import React from 'react';
 import Header from '../../common/Header';
 import Footer from '../../common/Footer';
 import Home from '../Home';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
 
 function Pages({ data }) {
     return (
         <>
-            <Router>
-                <Header />
-                <Switch>
-                    <Route path={process.env.PUBLIC_URL + '/'}>
-                        <Home data={data} />
-                    </Route>
-                </Switch>
-                <Footer />
-
-            </Router>
+            <Header />
+            <Home data={data} />
+            <Footer />
         </>
     );
 }
