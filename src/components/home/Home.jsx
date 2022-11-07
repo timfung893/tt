f73a9cc1e9fcfd6b7a13ba228835273edbcb7f3c
thinh <thinh.phung@apacpro.com>
2022-11-07 10:42:38 +0700

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import CardMain from './hero/CardMain';
 import Hero from './hero/Hero';
-import Blog from './blog/Blog';
 import Gallery from './product/Gallery';
 import Location from './Location';
 import Story from './Story';
@@ -9,11 +8,13 @@ import WeddDetails from './WeddDetails';
 import Celebrate from './Celebrate';
 
 function Home(props) {
-  const ios = /iPad|iPhone|iPod/.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
-  const cardSection = document.querySelector('.card-section')
-  if (ios === true && window.innerWidth < 544) {
-    cardSection.classList.add('scroll')
-  }
+  useEffect(() => {
+    const ios = /iPad|iPhone|iPod/.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+    const cardSection = document.querySelector('.card-section')
+    if (ios === true && window.innerWidth < 544) {
+      cardSection.classList.add('scroll')
+    }
+  })
 
   return (
     <>
