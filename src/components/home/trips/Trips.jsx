c30d65793f3343107c5764bb33d36a2875ff5f4b
thinh <thinh.phung@apacpro.com>
2023-12-21 10:27:52 +0700

    update

--- src/components/home/trips/Trips.jsx
+++ src/components/home/trips/Trips.jsx
@@ -6,35 +6,35 @@ import * as contentful from '../../../utils/contenful';
 
 
 function Trips(props) {
-    const [data, setData] = useState([]);
-    const getProducts = async () => {
-        const products = await contentful.client.getEntry('bH4QxqNLiYm3XDNnp88Pw');
-        let i = 0;
-        const data = Array.from(Array(products.fields.images.length), () => ({
-            id: i++,
-            cover: products.fields.images[i-1]
-        }));
+    // const [data, setData] = useState([]);
+    // const getProducts = async () => {
+    //     const products = await contentful.client.getEntry('bH4QxqNLiYm3XDNnp88Pw');
+    //     let i = 0;
+    //     const data = Array.from(Array(products.fields.images.length), () => ({
+    //         id: i++,
+    //         cover: products.fields.images[i-1]
+    //     }));
         
-        setData(data);
-    };
+    //     setData(data);
+    // };
 
-    useEffect(() => {
-        getProducts();
-    }, [])
+    // useEffect(() => {
+    //     getProducts();
+    // }, [])
 
     return (
         <>
             <section className="gallery" id='gallery'>
                 <div className="container">
                     <Heading title='Sài Gòn - Quảng Ngãi' desc='Về quê ăn tết 2023...' />
-                    <Masonry className="photos"
+                    {/* <Masonry className="photos"
                         items={data}
                         columnGutter={8}
                         columnWidth={150}
                         columnCount={3}
                         overscanBy={32}
                         render={PhotoCell}
-                    />
+                    /> */}
                     <Heading title='' desc='Còn nữa, nhưng để dành nha keke...' />
                 </div>
             </section>
